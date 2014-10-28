bd141496981c0c927b991ef8d8adc5a02c456341
Andy Hebrank <ahebrank@gmail.com>
2014-10-28 10:52:12 -0400

    initial working version

--- system/expressionengine/third_party/calendar_export/mcp.calendar_export.php
+++ system/expressionengine/third_party/calendar_export/mcp.calendar_export.php
@@ -58,36 +58,134 @@ class Calendar_export_mcp
     */
     public function index()
     {
-
-        // Title tag + breadcrumb
         $this->_cp_title('calendar_export_export');
-
         ee()->cp->set_breadcrumb(BASE . AMP . $this->module_url, ee()->lang->line('calendar_export_module_name'));
 
         ee()->load->library('javascript');
         ee()->cp->load_package_js('index');
 
-        $vars = array();
+        $this->_inject_javascript();
 
-        // $vars['groups_and_variables']     = ee()->republic_variables_model->get_groups_and_variables();
-        // $vars['variables']                = ee()->republic_variables_model->get_groupless_variables();
-        // $vars['groups']                   = ee()->republic_variables_model->get_empty_groups($vars['groups_and_variables']);
-        // $vars['module_url']               = $this->module_url;
-        // $vars['languages']                = $this->languages;
-        // $vars['settings']                 = $this->settings;
-        // $vars['ok_icon']                  = $this->theme_url.'republic_variables/images/ok.png';
-        // $vars['not_ok_icon']              = $this->theme_url.'republic_variables/images/cancel.png';
-        // $vars['module_access']            = $this->has_access();
-        // $vars['variable_edit_action_url'] = BASE . AMP . $this->module_url . AMP . 'method=update_variable' . AMP . 'variable_id=';
+        $vars = array();
+        $vars['message'] = "";
+        $vars['action_url'] = 'C=addons_modules'.AMP.'M=show_module_cp'.AMP.'module=calendar_export'.AMP.'method=index';
+
+        $submit = ee()->input->post('submit');
+        if ($submit) {
+            $filter = ee()->input->post('filter');
+            $events = $this->_get_events($filter);
+            if ($submit == "Export") {
+                // helpful filenaming
+                $label = array();
+                if ($filter['from_date']) {
+                    $label[] = "from-".$filter['from_date'];
+                }
+                if ($filter['to_date']) {
+                    $label[] = "to-".$filter['to_date'];
+                }
+                if (!empty($label)) {
+                    $label = "_" . implode("_", $label);
+                }
+                else {
+                    $label = "";
+                }
+                $this->_render_csv($events, $label);
+            }
+            $vars['events'] = $events;
+            $vars['filter'] = ee()->input->post('filter');
+        }
 
         return $this->_render('index', $vars);
     }
 
+    /***************************
+    * PROCESSING 
+    ****************************/
+
+    private function _get_events($filter) {
+
+        ee()->db->select('*');
+        ee()->db->from('calendar_events');
+
+        // filter
+        $where = array();
+        if ($filter['from_date']) {
+            $where['start_date >='] = $filter['from_date'];
+        }
+        if ($filter['to_date']) {
+            $where['end_date <='] = $filter['to_date'];
+        }
+        if (!empty($where)) {
+            ee()->db->where($where);
+        }
+
+        $results = ee()->db->get();
+        
+        if ($results->num_rows() > 0) {
+            // collect entry_ids
+            $entries = array();
+            foreach ($results->result_array() as $row) {
+                $entries[$row['entry_id']] = array(
+                    'start_date' => $row['start_date'],
+                    'end_date' => $row['end_date']);
+            }
+
+            $entries = $this->_get_entry_data($entries);
+            return $entries;
+        }
+
+        return false;
+    }
+
+    private function _get_entry_data($entries) {
+        // generate category lookup table
+        ee()->db->select('*');
+        ee()->db->from('categories');
+        $results = ee()->db->get();
+        $catmap = array();
+        foreach ($results->result_array() as $row) {
+            $catmap[$row['cat_id']] = $row['cat_name'];
+        }
+
+        // grab entry titles, etc.
+        $ids = array_keys($entries);
+        ee()->db->select('*');
+        ee()->db->from('channel_titles');
+        ee()->db->where_in('entry_id', $ids);
+        $results = ee()->db->get();
+
+        foreach ($results->result_array() as $row) {
+            $entries[$row['entry_id']]['title'] = $row['title'];
+
+            // get categories
+            ee()->db->select('*');
+            ee()->db->from('category_posts');
+            ee()->db->where('entry_id', $row['entry_id']);
+            $cats = ee()->db->get();
+            $catnames = array();
+            foreach ($cats->result_array() as $catrow) {
+                $catnames[] = $catmap[$catrow['cat_id']];
+            }
+
+            // stick a list of categories with each entry
+            $entries[$row['entry_id']]['categories'] = implode(", ", $catnames);
+        }
+
+        return $entries;
+    }
 
 
     /***************************
     * RENDERING
     ****************************/
+
+    private function _inject_javascript() {
+        ee()->cp->add_js_script(
+            array('ui' => array(
+                'core', 'datepicker'
+            )
+        ));
+    }
 
     private function _cp_title($title, $lang_value = true)
     {
@@ -118,5 +216,31 @@ class Calendar_export_mcp
 
         return ee()->load->view($view, $vars, true);
     }
+
+    private function _render_csv($events, $label) {
+        // generate output
+        $output = "ID,Title,Categories,Startdate,Enddate\n";
+
+        $quote_element = function($element) {
+            $element = str_replace('"', '\"', $element);
+            return '"' . $element . '"';
+        };
+
+        foreach ($events as $id=>$e) {
+            $output .= implode(",", 
+                array_map($quote_element, 
+                    array($id,$e['title'],$e['categories'],$e['start_date'],$e['end_date'])))
+                ."\n";
+        }
+        
+        // send csv attachment header
+        header("Content-type: text/csv");
+        header("Content-Disposition: attachment; filename=events_export".$label.".csv");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+        echo $output;
+        exit();
+    }
+
 }
 // END CLASS
--- system/expressionengine/third_party/calendar_export/views/index.php
+++ system/expressionengine/third_party/calendar_export/views/index.php
@@ -1,1 +1,44 @@
-<h1>Hello</h1>
+<section id="calendar_export">
+  <?=form_open($action_url, '', false)?>
+    <?php echo $message; ?>
+    <fieldset>
+      <legend>Date Range</legend>
+      <span class="date">
+        <?php 
+          $date_from  = isset($filter['from_date']) ? $filter['from_date'] : '';
+          $date_to  = isset($filter['to_date']) ? $filter['to_date'] : '';
+        ?>
+        <input name="filter[from_date]" type="text" class="datepicker" value="<?=$date_from?>"> - <input name="filter[to_date]" type="text" class="datepicker" value="<?=$date_to?>">
+      </span>
+    </fieldset>
+    <div>
+      <input name="submit" class="submit" type="submit" value="Preview"> 
+      <input name="submit" class="submit" type="submit" value="Export">
+    </div>
+  <?=form_close()?>
+
+  <?php if (isset($events)): ?>
+  <div>
+    <table class="events">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Categories</th>
+          <th>Start Date</th>
+          <th>End Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($events as $id=>$e): ?>
+        <tr>
+          <td><?=$e['title']?></td>
+          <td><?=$e['categories']?></td>
+          <td><?=date("Y-m-d", strtotime($e['start_date']))?></td>
+          <td><?=date("Y-m-d", strtotime($e['end_date']))?></td>
+        </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+  </div>
+  <?php endif; ?>
+</section>
